708f1acc40e3b35396f7334aafd09f33bfd33736
SamyKr <samyaymane@gmail.com>
2024-05-10 04:00:48 +0200

    Revert "A NE PAS PRENDRE EN COMPTE"
    This reverts commit a1d1e5929299771c85c6bf5608cc3b01898a273b.

--- CODE/intro.py
+++ CODE/intro.py
@@ -20,161 +20,6 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (QDialog, QVBoxLayout, QScrollArea, QWidget,
                              QLabel, QHBoxLayout, QCheckBox, QPushButton)
 from PyQt5.QtCore import pyqtSignal
-from monat import Dresseur,Starter,Rencontre,Soins
-
-pokemon_names_translation = {
-    "Bulbizarre": "Bulbasaur",
-    "Herbizarre": "Ivysaur",
-    "Florizarre": "Venusaur",
-    "Salameche": "Charmander",
-    "Reptincel": "Charmeleon",
-    "Dracaufeu": "Charizard",
-    "Carapuce": "Squirtle",
-    "Carabaffe": "Wartortle",
-    "Tortank": "Blastoise",
-    "Chenipan": "Caterpie",
-    "Chrysacier": "Metapod",
-    "Papilusion": "Butterfree",
-    "Aspicot": "Weedle",
-    "Coconfort": "Kakuna",
-    "Dardargnan": "Beedrill",
-    "Roucool": "Pidgey",
-    "Roucoups": "Pidgeotto",
-    "Roucarnage": "Pidgeot",
-    "Rattata": "Rattata",
-    "Rattatac": "Raticate",
-    "Piafabec": "Spearow",
-    "Rapasdepic": "Fearow",
-    "Abo": "Ekans",
-    "Arbok": "Arbok",
-    "Pikachu": "Pikachu",
-    "Raichu": "Raichu",
-    "Sabelette": "Sandshrew",
-    "Sablaireau": "Sandslash",
-    "Nidoran♀": "Nidoran♀",
-    "Nidorina": "Nidorina",
-    "Nidoqueen": "Nidoqueen",
-    "Nidoran♂": "Nidoran♂",
-    "Nidorino": "Nidorino",
-    "Nidoking": "Nidoking",
-    "Melofée": "Clefairy",
-    "Mélodelfe": "Clefable",
-    "Goupix": "Vulpix",
-    "Feunard": "Ninetales",
-    "Rondoudou": "Jigglypuff",
-    "Grodoudou": "Wigglytuff",
-    "Nosferapti": "Zubat",
-    "Nosferalto": "Golbat",
-    "Mystherbe": "Oddish",
-    "Ortide": "Gloom",
-    "Rafflesia": "Vileplume",
-    "Paras": "Paras",
-    "Parasect": "Parasect",
-    "Mimitoss": "Venonat",
-    "Aeromite": "Venomoth",
-    "Taupiqueur": "Diglett",
-    "Triopikeur": "Dugtrio",
-    "Miaouss": "Meowth",
-    "Persian": "Persian",
-    "Psykokwak": "Psyduck",
-    "Akwakwak": "Golduck",
-    "Ferosinge": "Mankey",
-    "Colossinge": "Primeape",
-    "Caninos": "Growlithe",
-    "Arcanin": "Arcanine",
-    "Ptitard": "Poliwag",
-    "Têtarte": "Poliwhirl",
-    "Tartard": "Poliwrath",
-    "Abra": "Abra",
-    "Kadabra": "Kadabra",
-    "Alakazam": "Alakazam",
-    "Machoc": "Machop",
-    "Machopeur": "Machoke",
-    "Mackogneur": "Machamp",
-    "Chétiflor": "Bellsprout",
-    "Boustiflor": "Weepinbell",
-    "Empiflor": "Victreebel",
-    "Tentacool": "Tentacool",
-    "Tentacruel": "Tentacruel",
-    "Racaillou": "Geodude",
-    "Gravalanch": "Graveler",
-    "Grolem": "Golem",
-    "Ponyta": "Ponyta",
-    "Galopa": "Rapidash",
-    "Ramoloss": "Slowpoke",
-    "Flagadoss": "Slowbro",
-    "Magneti": "Magnemite",
-    "Magnéton": "Magneton",
-    "Canarticho": "Farfetch'd",
-    "Doduo": "Doduo",
-    "Dodrio": "Dodrio",
-    "Otaria": "Seel",
-    "Lamantine": "Dewgong",
-    "Tadmorv": "Grimer",
-    "Grotadmorv": "Muk",
-    "Kokiyas": "Shellder",
-    "Crustabri": "Cloyster",
-    "Fantominus": "Gastly",
-    "Spectrum": "Haunter",
-    "Ectoplasma": "Gengar",
-    "Onix": "Onix",
-    "Soporifik": "Drowzee",
-    "Hypnomade": "Hypno",
-    "Krabby": "Krabby",
-    "Krabboss": "Kingler",
-    "Voltorbe": "Voltorb",
-    "Électrode": "Electrode",
-    "Nœunœuf": "Exeggcute",
-    "Noadkoko": "Exeggutor",
-    "Osselait": "Cubone",
-    "Ossatueur": "Marowak",
-    "Kicklee": "Hitmonlee",
-    "Tygnon": "Hitmonchan",
-    "Excelangue": "Lickitung",
-    "Smogo": "Koffing",
-    "Smogogo": "Weezing",
-    "Rhinocorne": "Rhyhorn",
-    "Rhinoferos": "Rhydon",
-    "Leveinard": "Chansey",
-    "Saquedeneu": "Tangela",
-    "Kangourex": "Kangaskhan",
-    "Hypotrempe": "Horsea",
-    "Hypocéan": "Seadra",
-    "Poissirène": "Goldeen",
-    "Poissoroy": "Seaking",
-    "Stari": "Staryu",
-    "Staross": "Starmie",
-    "M. Mime": "Mr. Mime",
-    "Insécateur": "Scyther",
-    "Lippoutou": "Jynx",
-    "Élektek": "Electabuzz",
-    "Magmar": "Magmar",
-    "Scarabrute": "Pinsir",
-    "Tauros": "Tauros",
-    "Magicarpe": "Magikarp",
-    "Léviator": "Gyarados",
-    "Lokhlass": "Lapras",
-    "Métamorph": "Ditto",
-    "Évoli": "Eevee",
-    "Aquali": "Vaporeon",
-    "Voltali": "Jolteon",
-    "Pyroli": "Flareon",
-    "Porygon": "Porygon",
-    "Amonita": "Omanyte",
-    "Amonistar": "Omastar",
-    "Kabuto": "Kabuto",
-    "Kabutops": "Kabutops",
-    "Ptéra": "Aerodactyl",
-    "Ronflex": "Snorlax",
-    "Artikodin": "Articuno",
-    "Électhor": "Zapdos",
-    "Sulfura": "Moltres",
-    "Minidraco": "Dratini",
-    "Draco": "Dragonair",
-    "Dracolosse": "Dragonite",
-    "Mewtwo": "Mewtwo",
-    "Mew": "Mew"
-}
 
 
 class MyDialog(QDialog):
@@ -196,31 +41,21 @@ class MyDialog(QDialog):
         pokedex_window.pokemon_selected.connect(self.start_game_with_selected_pokemon)
         pokedex_window.exec_()
         pokedex_window.close()  
-        
-    
 
     def start_game_with_selected_pokemon(self, selected_pokemon):
-        test=[]
         print("Pokémons sélectionnés:")
         for pokemon in selected_pokemon:
             print(f"Numéro: {pokemon['number']}, Nom: {pokemon['name']}")
-            
-            test.append(pokemon_names_translation[pokemon['name']])
         # Assigner selected_pokemon à l'attribut de classe
         self.selected_pokemon = selected_pokemon
-        print(test)
-        
         self.pokemon_list.add_starters(selected_pokemon)
         self.ui.pushButton.setText("GOOO !!")
         self.ui.pushButton.clicked.disconnect()  # Déconnexion de l'ancienne connexion
-        joueur = Dresseur("JOUEUR")
-        Starter(joueur,test)
-        self.ui.pushButton.clicked.connect(self.open_game_board(joueur))  # Nouvelle connexion
+        self.ui.pushButton.clicked.connect(self.open_game_board)  # Nouvelle connexion
 
-    def open_game_board(self,joueur):
+    def open_game_board(self):
         self.close()
-        print(self.selected_pokemon)
-        game_board = GameBoard(self.selected_pokemon,joueur)  # Passer selected_pokemon à GameBoard
+        game_board = GameBoard(self.selected_pokemon)  # Passer selected_pokemon à GameBoard
         game_board.exec()
 
 
